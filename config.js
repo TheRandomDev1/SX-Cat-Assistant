927d85f04fbcb0692f1a3470d88c124eee88d3de
SX Spy Agent <90962949+SX-9@users.noreply.github.com>
2022-02-04 19:37:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+exports.Prefix = "sx!";
+exports.Token = process.env.Token;
+exports.Color = "#00e1ff";
+exports.Server_Port = 2030;
+exports.Status_Text = " sx!help";
+exports.Status_Type = "LISTENING";
+exports.Ping_Message = "**Bot Links** \n> Bot Invite: https://bot.sx9.repl.co \n> Discord Community: https://invite.sx9.repl.co \n> Source Code: <https://github.com/SX-9/SX-Cat-Assistant> \n> Status Page: https://stats.uptimerobot.com/jB8ZDcYg5X/789642543";
+//put the token in the .env file make sure the key is "Token" and the value is your bot token!
